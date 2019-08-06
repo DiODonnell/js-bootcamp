5780bce22735bf6021d3ef36bbb0f732a4c122f8
DiODonnell <dianeodonnell7@gmail.com>
2019-08-07 10:11:10 +1200

    Using LET command to allow changes in input

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 //var name = "Di O'Donnell";
 //console.log(name);
-bla = 2;
-var bla;
-console.log(bla)
+//bla = 2;
+//var bla;
+//console.log(bla)
+
+let name = "Nelson"
+name = "NZ"
+console.log(name)
